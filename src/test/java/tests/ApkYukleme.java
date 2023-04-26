8108c6b7491ab7fbd7e60c6d54efbc6dae1d1a63
Sevilay <you@example.com>
2023-04-26 23:24:29 +0300

    28/04

--- src/test/java/tests/ApkYukleme.java
+++ src/test/java/tests/ApkYukleme.java
@@ -32,7 +32,7 @@ public class ApkYukleme {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,platformIsmi);
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,version);
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,automation);
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\sevil\\IdeaProjects\\Appium_T-108\\Apps\\all-currency-converter-3-9-0 (1).apk");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\sevil\\IdeaProjects\\Appium_T-108\\Apps\\Kiwi.com - Book Cheap Flights_2023.14.0_Apkpure.apk");
         driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
